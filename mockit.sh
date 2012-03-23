3394ffa34a14227892550fd84b4d9c8be971ac8e
Peter Briggs <peter.briggs@manchester.ac.uk>
2012-03-23 13:09:03 +0000

    mockit.sh: updated to pull in non-standard dependencies.
    If additional RPMs are specified on the command line then these are pulled into the mock build environment. This provides a way of building RPMs that are dependent on other RPMs which are not part of the offical repositories.

--- mockit.sh
+++ mockit.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 #
 # Build RPMs from spec file + sources using mock
+# It will also include any dependent RPMs explicitly specified on the
+# command line
 #
 # Collect target platform, SPEC file and sources location from command line
 target=$1
 spec=$2
 sources=$3
 if [ -z "$target" ] || [ -z "$spec" ] || [ -z "$sources" ] ; then
-    echo Usage: `basename $0` TARGET SPECFILE SOURCES
+    echo Usage: `basename $0` TARGET SPECFILE SOURCES [ RPM [ RPM... ] ]
     exit
 fi
 # Check inputs
@@ -23,10 +25,50 @@ if [ ! -d $sources ] ; then
     echo SOURCES directory $sources not found
     exit 1
 fi
+# Check dependencies
+dependencies=
+while [ ! -z $4 ] ; do
+    if [ -f $4 ] ; then
+	dependencies="$dependencies $4"
+    else
+	echo WARNING $4 not found
+    fi
+    shift
+done
+if [ -z "$dependencies" ] ; then
+    echo No dependencies specified/found
+fi
+# Create temporary directory
+wd=`mktemp -d`
+echo Temporary dir for cfg and mock-updates repo = $wd
+# Copy the cfg files to working directory
+cp /etc/mock/logging.ini $wd
+cp /etc/mock/site-defaults.cfg $wd
+cp /etc/mock/${target}.cfg $wd
+# Create a temporary repo with the specified RPMs
+if [ ! -z "$dependencies" ] ; then
+    mkdir ${wd}/mock-updates
+    for dep in $dependencies ; do
+	cp $dep ${wd}/mock-updates
+    done
+    createrepo -q ${wd}/mock-updates # use -q to suppress output
+    # Hack the cfg file to append the temporary repo
+    grep -v '^"""' ${wd}/${target}.cfg > ${wd}/${target}.cfg.tmp
+    mv ${wd}/${target}.cfg.tmp ${wd}/${target}.cfg
+    cat >> ${wd}/${target}.cfg <<EOF
+
+[mock-updates]
+name=mock-updates
+baseurl=file://${wd}/mock-updates
+gpgcheck=0
+"""
+EOF
+fi
 # Create SRPM
-mock -r $target --buildsrpm --spec $spec --sources $sources
+mock --configdir=$wd -r $target --buildsrpm --spec $spec --sources $sources
 if [ $? != 0 ] ; then
     echo mock exited with an error building SRPM
+    /bin/rm -rf ${wd}
     exit 1
 fi
 # Copy SRPM to central location
@@ -39,22 +81,27 @@ if [ -f "$srpm" ] ; then
     /bin/cp $srpm $HOME/mock/$target
 else
     echo Source RPM not found in /var/lib/mock/$target/result/
+    /bin/rm -rf ${wd}
     exit 1
 fi
 # Create RPMs from SRPM
-mock -r $target --rebuild $HOME/mock/$target/`basename $srpm`
+mock --configdir=$wd -r $target --rebuild $HOME/mock/$target/`basename $srpm`
 if [ $? != 0 ] ; then
     echo mock exited with an error building RPMs
+    /bin/rm -rf ${wd}
     exit 1
 fi
 # Copy RPMs to central location
 rpms=`ls /var/lib/mock/$target/result/*.rpm | grep -v debuginfo`
 if [ -z "$rpms" ] ; then
     echo No RPMs found in /var/lib/mock/$target/result/
+    /bin/rm -rf ${wd}
     exit 1
 fi
 for rpm in $rpms ; do
     echo $rpm
     /bin/cp $rpm $HOME/mock/$target
 done
+# Clean up temporary directory
+/bin/rm -rf ${wd}
 echo Done
